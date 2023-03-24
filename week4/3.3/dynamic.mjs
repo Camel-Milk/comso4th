367ca82d76ac32e27e2f12455f51e6c30dddfa86
Camel-Milk <unicon1218@gmail.com>
2023-03-24 21:19:27 +0900

    수정

--- week4/3.3/dynamic.mjs
+++ week4/3.3/dynamic.mjs
@@ -8,6 +8,6 @@ const a = true;
 if (a) {
     const m1 = await import('./func.mjs');
     console.log(m1);
-    const m2 = await import('./var.mjs');
+    const m2 = await import('./3.3-1/var.mjs');
     console.log(m2);
 }
